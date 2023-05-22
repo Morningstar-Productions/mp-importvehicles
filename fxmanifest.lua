19e1a9b87d3cb83f80c775ed7fbce0ccb3d1ba27
xViperAG <usairsoftingandgaming@gmail.com>
2023-05-22 17:47:12 -0400

    [dependency] Added Dependency and Resource Check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'cerulean'
 
-games { 'rdr3', 'gta5' }
+games { 'gta5' }
 
 files {
     'data/**/*.meta'
@@ -12,5 +12,10 @@ data_file 'CARCOLS_FILE' 'data/**/*.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/*.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/*.meta'
 data_file 'VEHICLE_SHOP_DLC_FILE' 'data/**/*.meta'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*.meta' 
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*.meta'
 
+server_file 'sv_resourcechecker.lua'
+
+dependecies {
+    'mp-vehicleaudio'
+}
